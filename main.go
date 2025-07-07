a2f8e6b868a068095dc2e466e9e46dfa4a3241da
ruhtar <arthur.amorim10@gmail.com>
2025-07-07 20:41:59 -0300

    add gin

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	r := gin.Default()
+
+	r.GET("", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Ok",
+		})
+	})
+
+	r.POST("/payments", func(c *gin.Context) {
+		var req PaymentRequest
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid payload"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message":       "Pagamento recebido com sucesso",
+			"correlationId": req.CorrelationID,
+			"amount":        req.Amount,
+		})
+	})
+
+	r.Run(":9999")
+}
+
+type PaymentRequest struct {
+	CorrelationID string  `json:"correlationId"`
+	Amount        float64 `json:"amount"`
 }
